3f6be31f1416589c647d7d6a3300ff6a0a1ea556
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-03-16 17:43:57 +0100

    Implement working version

--- index.js
+++ index.js
@@ -24,46 +24,69 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice){
     if (humanChoice === "rock" && computerChoice === "rock"){
         console.log("draw!");
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "rock" && computerChoice === "paper"){
         console.log("You loose! Paper beats rock!");
         computerScore ++;
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "rock" && computerChoice === "scissors"){
         console.log("You win! Rock beats paper!");
         humanScore ++;
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "paper" && computerChoice === "rock"){
         console.log("You win! Ppaer beats rock!");
         humanScore ++;
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "paper" && computerChoice === "paper"){
         console.log("draw!");
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "paper" && computerChoice === "scissors"){
         console.log("You loose! Scissors beats paper!");
         computerScore ++;
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "scissors" && computerChoice === "rock"){
         console.log("You loose! Rock beats scissors!");
         computerScore ++;
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "scissors" && computerChoice === "paper"){
         console.log("You win! Scissors beats paper!");
         humanScore ++;
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
     else if (humanChoice === "scissors" && computerChoice === "scissors"){
         console.log("draw!");
+        console.log(`The score is: ${humanScore} : ${computerScore}`);
     }
 }
 
-
+let roundCounter = 1;
 
 function playGame(){
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerchoice();
-    playRound(humanSelection, computerSelection);
-    console.log(`The core is: ${humanScore} : ${computerScore}`)
-}
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerchoice();
+    playRound(humanSelection, computerSelection); 
 
-// playGame();
+    if (roundCounter < 5){
+        roundCounter++; 
+        playGame();      
+    }
+    else{
+        roundCounter = 1;
+        if(humanScore > computerScore){
+            console.log("Consgrats! You won!");
+        }
+        else if (humanScore < computerScore){
+            console.log("Try next time! This time computer won!");
+        } 
+        else{
+            console.log("It's draw");
+        }
+    }
+}
